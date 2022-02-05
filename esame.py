06fa37c55bdaac73b55c7af4de898a85bd3231ac
Ruby-Betancourt <92112439+Ruby-Betancourt@users.noreply.github.com>
2022-02-05 14:52:51 +0000

    prove???

--- esame.py
+++ esame.py
@@ -1,40 +1,47 @@
 class ExamException(Exception):
     pass
 
-def compute_avg_monthly_difference(lista, start=None, end=None):
+def compute_avg_monthly_difference(lista, start, end):
     #inizio controlli
 
-    #if start is not None and end is not None:
-        #controllo l'anno iniziale (start)
-        #1 type
-        #if not isinstance(start, int):
-        #    raise ExamException('TypeError, start = '+ start +' non è un intero ma è di tipo '+ type(start))
-        #2 segno
-        #if start<0:
-        #    raise ExamException('Error, start è negativo')
-                 
-        #controllo l'anno finale (end)
-        #1 type
-        #if not isinstance(end, int):
-        #    raise ExamException('TypeError, end = '+ end +' non è un intero ma è di tipo '+ type(end))
-        #2 segno
-        #if end<0:
-        #    raise ExamException('Error, end è negativo')
+    #controllo l'anno iniziale (start)
+    #1 type
+    #if not isinstance(start, int):
+    #    raise ExamException('TypeError, start non è di tipo int')
+    #2 segno
+    #if start<0:
+    #    raise ExamException('Error, start è negativo')
+             
+    #controllo l'anno finale (end)
+    #1 type
+    #if not isinstance(end, int):
+    #    raise ExamException('TypeError, end non è di tipo int')
+    #2 segno
+    #if end<0:
+    #    raise ExamException('Error, end è negativo')
 
-        #3 controllo ordine
-        #if start>end:
-        #    raise ExamException('Error, forse hai invertito start e end, start non può essere maggiore di end')
+    #3 controllo ordine
+    #if start>end:
+    #    raise ExamException('Error, forse hai invertito start e end, start non può essere maggiore di end')
 
     #inizio funzione
         lista_finale = []
         media = 0
-        list_anno = []
+        t=end-start
+
+        for i in range t:
+            list_anno = []
+            list_anno.append(start+i)
+        
+        lis_year = []
+
         for el in lista:
             elem = el[0].split('-')
-            if elem[0] == start:
-                list_anno.append(el[1])
+            if int(elem[0]) == start:
+                lis_year.append(el[1])
+                
     
-        return list_anno
+        return lista_finale
         #for el in lista:
                     
 
@@ -70,5 +77,5 @@ time_series_file = CVSTimeSeriesFile(name='data.csv')
 time_series = time_series_file.get_data() 
 #print(time_series) 
 
-media = compute_avg_monthly_difference(time_series, '1949', '1951')
+media = compute_avg_monthly_difference(time_series, 1949, 1951)
 print(media)
---
+++ prove.py
@@ -0,0 +1,22 @@
+lista=[]
+start = 1949
+end = 1951
+t=end-start
+
+for i in range (t):
+    lis = []
+    lis.append(start+i)
+    lista.append(lis)
+
+listaa = [1949, 2,4,5]
+listab = [1950, 5,6,7]
+listac = [1951, 7,8,9]
+
+som = 0
+c = end
+while c!=0:
+    
+    diff=listac[1]-listab[1]
+
+
+print (lista)
