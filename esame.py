71172da1e322fc6f9be130289f107c7d1eded739
Ruby-Betancourt <92112439+Ruby-Betancourt@users.noreply.github.com>
2022-02-08 13:49:45 +0000

    liste fatt

--- esame.py
+++ esame.py
@@ -1,8 +1,52 @@
 class ExamException(Exception):
     pass
+                    
+
+class CVSTimeSeriesFile():
+    #inizializzo la classe
+    def __init__(self, name):
+        self.name = name
+        
+        #controllo che sia una stringa
+        if not isinstance(self.name, str):
+            raise ExamException('TypeError, il nome "{}" non è una stringa'. format(self.name))
+
+
+    def get_data(self):
+        #inizializzo la futura lista di liste
+        finish_list = []
+
+        #apro il file
+        try:
+            my_file = open(self.name, 'r')
+        except:
+            raise ExamException('NotFoundError, file non esiste o non è legibile')    
+
+        for line in my_file:
+            #split gli elementi 
+            elements = line.split(',')
+
+            #elimino il carattere '\n'
+            elements[-1] = elements[-1].strip()
+
+            if elements[0] != 'date':
+                #trasformo tutti i dati del passeggeri in int
+                elements[1] = int(elements[1])
+                #aggiungo ogni lista nella lista finale
+                finish_list.append(elements)  
+
+
+        #chiudo il file e return la lista di liste
+        my_file.close()
+        return finish_list 
+
+
 
 def compute_avg_monthly_difference(lista, start, end):
     #inizio controlli
+    
+    start=int(start)
+    end=int(end)
 
     #controllo l'anno iniziale (start)
     #1 type
@@ -24,69 +68,42 @@ def compute_avg_monthly_difference(lista, start, end):
     #if start>end:
     #    raise ExamException('Error, forse hai invertito start e end, start non può essere maggiore di end')
 
+
     #inizio funzione
-        lista_finale = []
-        media = 0
-        t=end-start
-        temp = []
+    lista_finale = []
+    media = 0
+    t=end-start
+    temp = []
+    somma=[]
         
-        for i in range (t+1):
-            list_anno = []
-            list_anno.append(start+i)
-            temp.append(list_anno)
+    for i in range (t+1):
+        list_anno = []
+        list_anno.append(start+i)
+        temp.append(list_anno)
         
-        lis_year = []
-        for el in lista:
-            elem = el[0].split('-')
-            if int(elem[0]) == start:
-                lis_year.append(el[1])
+    #lis_year = []
+    #for el in lista:
+    #    elem = el[0].split('-')
+    #    if int(elem[0]) == start:
+    #        lis_year.append(el[1])
 
-        c = end
-        i=0
+        
+    for dat in temp:
         for el in lista:
             elem = el[0].split('-')
-            if int(elem[0]) == c:
-                temp[i].append(el[1])
-            
-            c-=1  
-            i+=1     
-                       
+        
+            if int(elem[0]) == dat[0]:
+                dat.append(el[1])  
+                 
     
-        return temp
 
-                    
-
-class CVSTimeSeriesFile():
-    #inizializzo la classe
-    def __init__(self, name):
-        self.name = name
-        #controllo che sia una stringa
-        if not isinstance(self.name, str):
-            raise ExamException('TypeError, il nome "{}" non è una stringa'. format(self.name))
+    return temp
 
 
-    def get_data(self):
-        #inizializzo la futura lista di liste
-        finish_list = []
-
-        #apro il file
-        my_file = open(self.name, 'r')
-
-        for line in my_file:
-            #split gli elementi 
-            elements = line.split(',')
-
-            #aggiungo ogni lista nella lista finale
-            if elements[0] != 'date':
-                finish_list.append(elements)  
-
-        #chiudo il file e return la lista di liste
-        my_file.close()
-        return finish_list 
 
 time_series_file = CVSTimeSeriesFile(name='data.csv')
 time_series = time_series_file.get_data() 
 #print(time_series) 
 
-media = compute_avg_monthly_difference(time_series, 1949, 1951)
+media = compute_avg_monthly_difference(time_series, '1949', '1951')
 print(media)
