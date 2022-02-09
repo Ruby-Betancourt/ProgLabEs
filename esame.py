e20b617b2fac3064d8643bbc184c44134017b6b0
Ruby-Betancourt <92112439+Ruby-Betancourt@users.noreply.github.com>
2022-02-09 15:30:06 +0000

    Eccezioni~~

--- esame.py
+++ esame.py
@@ -1,6 +1,7 @@
+from datetime import datetime
+
 class ExamException(Exception):
-    pass
-                    
+    pass                  
 
 class CVSTimeSeriesFile():
     #inizializzo la classe
@@ -16,20 +17,25 @@ class CVSTimeSeriesFile():
         #inizializzo la futura lista di liste
         finish_list = []
 
-        #apro il file
+        #provo ad aprire il file
         try:
             my_file = open(self.name, 'r')
         except:
+            #in caso non riesca ad aprire il file
             raise ExamException('NotFoundError, file non esiste o non è legibile')    
 
         for line in my_file:
-            #split gli elementi 
+            #separo la stringa sulla virgola 
             elements = line.split(',')
-
+ 
             #elimino il carattere '\n'
             elements[-1] = elements[-1].strip()
 
             if elements[0] != 'date':
+                #controllo che i dati passeggeri si possano trasformare in intero
+                if not elements[1].isdigit():
+                    raise ExamException('Error, {} non è un numero velodo per la media'. format(elements[1]))
+
                 #trasformo tutti i dati del passeggeri in int
                 elements[1] = int(elements[1])
                 #aggiungo ogni lista nella lista finale
@@ -43,75 +49,81 @@ class CVSTimeSeriesFile():
 
 
 def compute_avg_monthly_difference(lista, start, end):
-    #inizio controlli
-    
-    #controllo che sia una stringa
-    #isinstance()
+    #controlli sull'anno iniziale (start) e finale (end)
 
-    #controllo che non sia una stringa vuota
+    #controllo che siano una stringa
+    if not isinstance(start, str):
+        raise ExamException('TypeError, l\'anno iniziale non è una stringa')
 
-    #controllo che sia una stringa di numeri
-    #isdigit()
+    if not isinstance(end, str):
+        raise ExamException('TypeError, l\'anno finale non è una stringa')
 
+    #controllo che non siano una stringa vuota
+    if start == '':
+        raise ExamException('Error, l\'anno iniziale è una stringa vuota')
+
+    if end == '':
+        raise ExamException('Error, l\'anno finale è una stringa vuota')
+
+    #controllo che siano una stringa di solo numeri
+    if not start.isdigit():
+        raise ExamException('Error, l\'anno iniziale non è un numero intero')
+
+    if not end.isdigit():
+        raise ExamException('Error, l\'anno finale non è un numero intero')    
+
+
+    #trasformo le date in interi
     start=int(start)
     end=int(end)
 
-    #controllo l'anno iniziale (start)
-    #1 type
-    #if not isinstance(start, int):
-    #    raise ExamException('TypeError, start non è di tipo int')
-    #2 segno
-    #if start<0:
-    #    raise ExamException('Error, start è negativo')
-             
-    #controllo l'anno finale (end)
-    #1 type
-    #if not isinstance(end, int):
-    #    raise ExamException('TypeError, end non è di tipo int')
-    #2 segno
-    #if end<0:
-    #    raise ExamException('Error, end è negativo')
 
-    # controllo che start non sia minore di 1949 cioe la prima data
+    #controllo che siano compresi nel file 
+    if start<1949:
+        raise ExamException('Error, l\'anno iniziale non compare nel file')
 
-    #3 controllo ordine
-    #if start>end:
-    #    raise ExamException('Error, forse hai invertito start e end, start non può essere maggiore di end')
+    if end>1960:
+        raise ExamException('Error, l\'anno finale non compare nel file')
 
+    #controllo che l'ordine delle date sia giusto
+    if start>end:
+        raise ExamException('Error, forse hai invertito start e end, start non può essere maggiore di end')
 
 
     #inizio funzione
     lista_finale = []
-    media = 0
-    t=end-start
-    temp = []
+    tot_anni = end-start
+    passengers = []
     somma=0
-        
-    for i in range (t+1):
-        list_anno = []
-        list_anno.append(start+i)
-        temp.append(list_anno)
-        
-    #lis_year = []
-    #for el in lista:
-    #    elem = el[0].split('-')
-    #    if int(elem[0]) == start:
-    #        lis_year.append(el[1])
-
-        
-    for dat in temp:
-        for el in lista:
-            elem = el[0].split('-')
-        
-            if int(elem[0]) == dat[0]:
-                dat.append(el[1])  
-
     m=1
+
+    #creo una lista per ogni anno che devo considerare    
+    for i in range (tot_anni+1):
+        lista_anno = []
+        #il primo elemento di ogni lista è l'anno 
+        lista_anno.append(start+i)
+        passengers.append(lista_anno)
+
+    #riempo le liste con i dati dei passeggeri     
+    for data in passengers:
+        for item in lista:
+            elem = item[0].split('-')
+            #solo se l'anno corrisponde all'anno nella lista i dati vengono aggiunti
+            if int(elem[0]) == data[0]:
+                data.append(item[1])  
+
+    #controlli sui dati dei passeggeri
+    for item in passengers:
+        for element in item:
+            
+
+    #calcolo la media per ogni mese
     while m<=12:
-        for j in range(t):
-            diff = temp[j+1][m]-temp[j][m]
+        for y in range(tot_anni):
+
+            diff = passengers[y+1][m]-passengers[y][m]
             somma+=diff
-        lista_finale.append(somma/t)
+        lista_finale.append(somma/tot_anni)
         somma=0
         m+=1    
 
@@ -124,4 +136,4 @@ time_series = time_series_file.get_data()
 #print(time_series) 
 
 media = compute_avg_monthly_difference(time_series, '1949', '1951')
-print(media)
+print(media) 
